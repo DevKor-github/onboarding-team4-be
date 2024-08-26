6563a629cc4f5fcba04da71d23f6fa426abd77e4
Na1gae <nalgae@korea.ac.kr>
2024-08-26 18:22:06 +0900

    Fix: 불필요한 Service 제거

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,11 @@
 import { BadRequestException, Body, Controller, ForbiddenException, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { FileService } from 'src/file/file.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('auth')
 export class AuthController {
     constructor(
-        private readonly authService: AuthService,
-        private readonly fileService: FileService
+        private readonly authService: AuthService
     ){}
 
     @Post('captcha')
@@ -29,7 +27,6 @@ export class AuthController {
     }
 
     @Post('signup')
-    @UseInterceptors(FileInterceptor('profileImage'))
     async signup(
         @Body('userId') userId: string,
         @Body('password') password: string,
